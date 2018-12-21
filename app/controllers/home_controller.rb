c1a57ea9f499163d45a97b1474944278494b8971
Prashant-Kumar-XFT <prashant_kumar_xft@external.mckinsey.com>
2018-12-21 14:56:25 +0530

    updated website code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,38 @@
 class HomeController < ApplicationController
   def index
   end
+
+
+  def current_committee
+
+  end
+
+  def sub_committee
+
+  end
+
+  def members
+
+  end
+  def about_our_puja
+
+  end
+
+  def support_us
+    #@support= Support.new
+    unless params.nil?
+      workbook = Roo::Spreadsheet.open './mpct_about_us.xlsx'
+      worksheets = workbook.sheets.last
+      workbook.sheet(worksheets).each_row_streaming do |rows|
+        rows.map do|row|
+          row.value
+        end
+      end
+
+    end
+  end
+
+  def contacts_us
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  get 'homes', to:'home#index'
+  get 'contacts_us', to:'home#contacts_us'
+  get 'current_committee', to:'home#current_committee'
+  get 'sub_committee', to:'home#sub_committee'
+  get 'members', to:'home#members'
+  get 'about_our_puja', to:'home#about_our_puja'
+  get 'support_us', to:'home#support_us'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
